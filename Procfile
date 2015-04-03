bfbdef60823238d0330b72f4aae4e71259a4e5cc
Rafael Garcia <rgarcia2009@gmail.com>
2015-04-03 10:51:46 -0700

    heroku-ify

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: scheduleforlater.com
--- server.go
+++ server.go
@@ -5,14 +5,18 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	var port string
+	if port = os.Getenv("PORT"); port == "" {
+		log.Fatal("must set PORT")
+	}
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello", html.EscapeString(r.URL.Path))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }

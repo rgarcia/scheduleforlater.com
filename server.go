596b57715bf99f5380d8e7f629c73e7e2a54d1e1
Rafael Garcia <rgarcia2009@gmail.com>
2015-04-05 20:43:51 -0700

    nux: set preferences

--- server.go
+++ server.go
@@ -57,12 +57,13 @@ type User struct {
 	LastName  string        `bson:"last_name"`
 	Email     string        `bson:"email"`
 	GCal      struct {
+		Email        string `bson:"email"`
 		AccessToken  string `bson:"access_token"`
 		RefreshToken string `bson:"refresh_token"`
 	} `bson:"gcal"`
 	Prefs struct {
-		StartTime *time.Time `bson:"start_time"`
-		EndTime   *time.Time `bson:"end_time"`
+		StartTime time.Time `bson:"start_time"`
+		EndTime   time.Time `bson:"end_time"`
 	}
 }
 
@@ -270,8 +271,8 @@ func main() {
 		}
 
 		// present google auth link
-		fmt.Fprintf(w, "<html><body>Thanks! Please connect this email (%s) with Google Calendar by clicking <a href='%s'>here</a>.</body></html>",
-			verification.Email, oauth2config.AuthCodeURL(session.Values["googleOAuth2State"].(string), oauth2.AccessTypeOffline))
+		fmt.Fprintf(w, "<html><body>Thanks! Please connect this email (%s) with the Google Calendar account you'd like to use by clicking <a href='%s'>here</a>.</body></html>",
+			verification.Email, oauth2config.AuthCodeURL(session.Values["googleOAuth2State"].(string), oauth2.AccessTypeOffline, oauth2.ApprovalForce))
 	})
 
 	http.HandleFunc("/oauth2/google", func(w http.ResponseWriter, r *http.Request) {
@@ -308,8 +309,260 @@ func main() {
 			return
 		}
 
-		// ask user to set some stuff
+		// ask user to set preferences
+		fmt.Fprintf(w, `<html><body>Almost there.
+Please give us the earliest and latest time you'd like to schedule something.
+<br/><br/>
+<form name="userprefs" action="/setup" method="POST">
+  Start time:
+  <select name="start-time">
+    <option value="12:00am">12:00am</option>
+    <option value="12:30am">12:30am</option>
+    <option value="1:00am">1:00am</option>
+    <option value="1:30am">1:30am</option>
+    <option value="2:00am">2:00am</option>
+    <option value="2:30am">2:30am</option>
+    <option value="3:00am">3:00am</option>
+    <option value="3:30am">3:30am</option>
+    <option value="4:00am">4:00am</option>
+    <option value="4:30am">4:30am</option>
+    <option value="5:00am">5:00am</option>
+    <option value="5:30am">5:30am</option>
+    <option value="6:00am">6:00am</option>
+    <option value="6:30am">6:30am</option>
+    <option value="7:00am">7:00am</option>
+    <option value="7:30am">7:30am</option>
+    <option value="8:00am">8:00am</option>
+    <option value="8:30am">8:30am</option>
+    <option value="9:00am" selected="selected">9:00am</option>
+    <option value="9:30am">9:30am</option>
+    <option value="10:00am">10:00am</option>
+    <option value="10:30am">10:30am</option>
+    <option value="11:00am">11:00am</option>
+    <option value="11:30am">11:30am</option>
+    <option value="12:00pm">12:00pm</option>
+    <option value="12:30pm">12:30pm</option>
+    <option value="1:00pm">1:00pm</option>
+    <option value="1:30pm">1:30pm</option>
+    <option value="2:00pm">2:00pm</option>
+    <option value="2:30pm">2:30pm</option>
+    <option value="3:00pm">3:00pm</option>
+    <option value="3:30pm">3:30pm</option>
+    <option value="4:00pm">4:00pm</option>
+    <option value="4:30pm">4:30pm</option>
+    <option value="5:00pm">5:00pm</option>
+    <option value="5:30pm">5:30pm</option>
+    <option value="6:00pm">6:00pm</option>
+    <option value="6:30pm">6:30pm</option>
+    <option value="7:00pm">7:00pm</option>
+    <option value="7:30pm">7:30pm</option>
+    <option value="8:00pm">8:00pm</option>
+    <option value="8:30pm">8:30pm</option>
+    <option value="9:00pm">9:00pm</option>
+    <option value="9:30pm">9:30pm</option>
+    <option value="10:00pm">10:00pm</option>
+    <option value="10:30pm">10:30pm</option>
+    <option value="11:00pm">11:00pm</option>
+    <option value="11:30pm">11:30pm</option>
+  </select>
+  <select name="start-time-zone">
+    <option value="-1200">-1200</option>
+    <option value="-1100">-1100</option>
+    <option value="-1000">-1000</option>
+    <option value="-0900">-0900</option>
+    <option value="-0700" selected="selected">-0700</option>
+    <option value="-0600">-0600</option>
+    <option value="-0500">-0500</option>
+    <option value="-0400">-0400</option>
+    <option value="-0300">-0300</option>
+    <option value="-0200">-0200</option>
+    <option value="-0100">-0100</option>
+    <option value="+0000">+0000</option>
+    <option value="+0100">+0100</option>
+    <option value="+0200">+0200</option>
+    <option value="+0300">+0300</option>
+    <option value="+0400">+0400</option>
+    <option value="+0500">+0500</option>
+    <option value="+0600">+0600</option>
+    <option value="+0700">+0700</option>
+    <option value="+0800">+0800</option>
+    <option value="+0900">+0900</option>
+    <option value="+1000">+1000</option>
+    <option value="+1100">+1100</option>
+    <option value="+1200">+1200</option>
+  </select>
+  <br/><br/>
+  End time:
+  <select name="end-time">
+    <option value="12:00am">12:00am</option>
+    <option value="12:30am">12:30am</option>
+    <option value="1:00am">1:00am</option>
+    <option value="1:30am">1:30am</option>
+    <option value="2:00am">2:00am</option>
+    <option value="2:30am">2:30am</option>
+    <option value="3:00am">3:00am</option>
+    <option value="3:30am">3:30am</option>
+    <option value="4:00am">4:00am</option>
+    <option value="4:30am">4:30am</option>
+    <option value="5:00am">5:00am</option>
+    <option value="5:30am">5:30am</option>
+    <option value="6:00am">6:00am</option>
+    <option value="6:30am">6:30am</option>
+    <option value="7:00am">7:00am</option>
+    <option value="7:30am">7:30am</option>
+    <option value="8:00am">8:00am</option>
+    <option value="8:30am">8:30am</option>
+    <option value="9:00am">9:00am</option>
+    <option value="9:30am">9:30am</option>
+    <option value="10:00am">10:00am</option>
+    <option value="10:30am">10:30am</option>
+    <option value="11:00am">11:00am</option>
+    <option value="11:30am">11:30am</option>
+    <option value="12:00pm">12:00pm</option>
+    <option value="12:30pm">12:30pm</option>
+    <option value="1:00pm">1:00pm</option>
+    <option value="1:30pm">1:30pm</option>
+    <option value="2:00pm">2:00pm</option>
+    <option value="2:30pm">2:30pm</option>
+    <option value="3:00pm">3:00pm</option>
+    <option value="3:30pm">3:30pm</option>
+    <option value="4:00pm">4:00pm</option>
+    <option value="4:30pm">4:30pm</option>
+    <option value="5:00pm" selected="selected">5:00pm</option>
+    <option value="5:30pm">5:30pm</option>
+    <option value="6:00pm">6:00pm</option>
+    <option value="6:30pm">6:30pm</option>
+    <option value="7:00pm">7:00pm</option>
+    <option value="7:30pm">7:30pm</option>
+    <option value="8:00pm">8:00pm</option>
+    <option value="8:30pm">8:30pm</option>
+    <option value="9:00pm">9:00pm</option>
+    <option value="9:30pm">9:30pm</option>
+    <option value="10:00pm">10:00pm</option>
+    <option value="10:30pm">10:30pm</option>
+    <option value="11:00pm">11:00pm</option>
+    <option value="11:30pm">11:30pm</option>
+  </select>
+  <select name="end-time-zone">
+    <option value="-1200">-1200</option>
+    <option value="-1100">-1100</option>
+    <option value="-1000">-1000</option>
+    <option value="-0900">-0900</option>
+    <option value="-0700" selected="selected">-0700</option>
+    <option value="-0600">-0600</option>
+    <option value="-0500">-0500</option>
+    <option value="-0400">-0400</option>
+    <option value="-0300">-0300</option>
+    <option value="-0200">-0200</option>
+    <option value="-0100">-0100</option>
+    <option value="+0000">+0000</option>
+    <option value="+0100">+0100</option>
+    <option value="+0200">+0200</option>
+    <option value="+0300">+0300</option>
+    <option value="+0400">+0400</option>
+    <option value="+0500">+0500</option>
+    <option value="+0600">+0600</option>
+    <option value="+0700">+0700</option>
+    <option value="+0800">+0800</option>
+    <option value="+0900">+0900</option>
+    <option value="+1000">+1000</option>
+    <option value="+1100">+1100</option>
+    <option value="+1200">+1200</option>
+  </select>
+  <br/><br/>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
+`)
+	})
+
+	http.HandleFunc("/setup", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			w.Header().Set("Allow", "POST")
+			http.Error(w, "only POST method is allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		if err := r.ParseForm(); err != nil {
+			log.Printf("invalid form: %s", err)
+			http.Error(w, "invalid form", http.StatusBadRequest)
+			return
+		}
+
+		// validate form data
+		startTime := r.FormValue("start-time")
+		startTimeZ := r.FormValue("start-time-zone")
+		endTime := r.FormValue("end-time")
+		endTimeZ := r.FormValue("end-time-zone")
+		if startTime == "" || startTimeZ == "" || endTime == "" || endTimeZ == "" {
+			fmt.Fprintf(w, "Invalid submission. Hit back and try again.")
+			return
+		}
+		start, err := time.Parse("3:00pm -0700", fmt.Sprintf("%s %s", startTime, startTimeZ))
+		if err != nil {
+			log.Printf("error parsing start time: %s", err)
+			http.Error(w, "Internal error. Please go back and try again.", http.StatusInternalServerError)
+			return
+		}
+		end, err := time.Parse("3:00pm -0700", fmt.Sprintf("%s %s", endTime, endTimeZ))
+		if err != nil {
+			log.Printf("error parsing end time: %s", err)
+			http.Error(w, "Internal error. Please go back and try again.", http.StatusInternalServerError)
+			return
+		}
+		if start.Equal(end) {
+			http.Error(w, "Times must be different. Please go back and try again.", http.StatusBadRequest)
+			return
+		}
+		if start.After(end) {
+			http.Error(w, "Start time must come after end time. Please go back and try again.", http.StatusBadRequest)
+			return
+		}
+
+		// create user
+		session, _ := sessionstore.Get(r, sessionkey)
+		client := oauth2config.Client(ctx, &oauth2.Token{AccessToken: session.Values["access_token"].(string)})
+		svc, err := googlebasic.New(client)
+		if err != nil {
+			log.Printf("error initiating service: %s", err)
+			http.Error(w, "Internal error. Please go back and try again.", http.StatusInternalServerError)
+			return
+		}
+		userinfo, err := googlebasic.NewUserinfoV2Service(svc).Me.Get().Do()
+		if err != nil {
+			log.Printf("error calling userinfo service: %s", err)
+			http.Error(w, "Internal error. Please go back and try again.", http.StatusInternalServerError)
+			return
+		}
+		user := User{
+			ID:        bson.NewObjectId(),
+			FirstName: userinfo.Given_name,
+			LastName:  userinfo.Family_name,
+			Email:     session.Values["email"].(string),
+		}
+		user.GCal.Email = userinfo.Email
+		user.GCal.AccessToken = session.Values["access_token"].(string)
+		user.GCal.RefreshToken = session.Values["refresh_token"].(string)
+		user.Prefs.StartTime = start
+		user.Prefs.EndTime = end
+
+		if user.Email == "" || user.GCal.Email == "" || user.GCal.AccessToken == "" || user.GCal.RefreshToken == "" {
+			log.Printf("user invalid: %#v", user)
+			http.Error(w, "Internal error. Please go back and try again.", http.StatusInternalServerError)
+			return
+		}
+		log.Printf("creating new user: %#v", user)
+		if err := mgosession.DB("").C("users").Insert(user); err != nil {
+			log.Printf("unable to insert user: %s", err)
+			http.Error(w, "Internal error. Please go back and try again.", http.StatusInternalServerError)
+			return
+		}
+
+		// process pending email that triggered setup process
 		// TODO
+
+		fmt.Fprintf(w, "Thanks! You're all set.")
 	})
 
 	http.HandleFunc("/dumpsession", func(w http.ResponseWriter, r *http.Request) {
